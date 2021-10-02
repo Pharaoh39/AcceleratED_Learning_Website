11025c416f7eedc0c7fff3a1d24178d295943a2d
baberle <77088315+baberle@users.noreply.github.com>
2021-10-01 20:05:12 -0400

    Created basic website as starting point

---
+++ AcceleratED_Learning_Website/scripts/video_chat.js
@@ -0,0 +1,11 @@
+var video = document.querySelector("#video_element");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Problem with video");
+    });
+}
